7bd7efc2f55405fb1b6320032dfb3cc4b70dbf92
Arannyak Roy <officialroy02@gmail.com>
2024-03-03 00:47:21 +0530

    Create basic UI and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipes/recipe-list/recipe/recipe.component';
+import { NewIngredientComponent } from './shopping-list/new-ingredient/new-ingredient.component';
+import { IngredientListComponent } from './shopping-list/ingredient-list/ingredient-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
+    NewIngredientComponent,
+    IngredientListComponent,
+    RecipeListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shopping-list/new-ingredient/new-ingredient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-ingredient',
+  templateUrl: './new-ingredient.component.html',
+  styleUrl: './new-ingredient.component.css'
+})
+export class NewIngredientComponent {
+
+}
